12f2226a9a4a8aeb986d6881d77d04eaca92a3df
Mahmoud <mhablas4@gmail.com>
2026-03-15 03:50:46 -0700

    Add divide function

--- calculator.py
+++ calculator.py
@@ -11,6 +11,10 @@ def multiply(a, b):
     """Return the product of a and b."""
     return a * b
 
+def divide(a, b):
+    """Return the quotient of a and b."""
+    return a / b
+
 if __name__ == "__main__":
     print("Addition: ", add(10, 5))
     print("Subtraction: ", subtract(10, 5))
